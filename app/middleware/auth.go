c70c94dd033336d88c35af873f306ab5ab653e1d
adeptventures <al.kindi.i@mail.ugm.ac.id>
2023-05-12 20:24:30 +0700

    fix error

--- app/middleware/auth.go
+++ app/middleware/auth.go
@@ -6,11 +6,11 @@ import (
 
 func Auth() gin.HandlerFunc {
 	return gin.HandlerFunc(func(ctx *gin.Context) {
-		claims := &Claims{}
-		token, err := ctx.Cookie("session_token")
-		if err != nil {
-			jwt
-		}
+		// claims := &Claims{}
+		// token, err := ctx.Cookie("session_token")
+		// if err != nil {
+		// 	jwt
+		// }
 		// TODO: answer here
 	})
 }
